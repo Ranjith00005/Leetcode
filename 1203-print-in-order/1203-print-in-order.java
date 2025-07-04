87840438a3a5c8636a4ba5be64d6d1fa461ea67e
Ranjith00005 <ranjithsme1@gmail.com>
2025-07-04 10:18:49 +0530

    Time: 11 ms (80.37%), Space: 42.4 MB (48.19%) - LeetHub

---
+++ 1203-print-in-order/1203-print-in-order.java
@@ -0,0 +1,29 @@
+class Foo {
+    private Semaphore two;
+    private Semaphore three;
+
+    public Foo() {
+        two=new Semaphore(0);
+        three= new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        two.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        two.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        three.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        three.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
