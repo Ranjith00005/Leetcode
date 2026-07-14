9bbf66bdb21bda2c458afc6d9ca378db73fbe922
Ranjith00005 <ranjithsme1@gmail.com>
2026-07-14 22:16:51 +0530

    Time: 13 ms (98.97%), Space: 51.5 MB (55.98%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,9 +2,10 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int count=0;
         int i=0;
         int j=0;
+        int count=0;
+
         while(i<g.length && j<s.length){
             if(s[j]>=g[i]){
                 i++;
@@ -12,6 +13,10 @@ class Solution {
             }
             j++;
         }
+
         return count;
+
+
+
     }
 }
